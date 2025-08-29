bd648ed3e50261102a280ab2a0ec5a70a30c1663
MarcpTr <marcphtr@gmail.com>
2025-08-29 21:57:41 +0200

    updated

--- src/main/java/com/tweetarchive/main/config/SecurityConfig.java
+++ src/main/java/com/tweetarchive/main/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.tweetarchive.main.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,23 +13,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login","/","/collection/**","/search","/error","/user/**").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/register", "/login", "/", "/collection/**", "/search", "/error", "/user/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
-                        .permitAll()
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/", true)
+                                .permitAll()
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                        .deleteCookies("JSESSIONID", "remember-me")
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/")
+                                .deleteCookies("JSESSIONID", "remember-me")
                 ).rememberMe(remember -> remember
-                        .key("super-secret-key") // Clave para firmar las cookies
-                        .tokenValiditySeconds(604800).alwaysRemember(false)
+                                .key("super-secret-key")
+                                .tokenValiditySeconds(604800).alwaysRemember(false)
                 );
 
         return http.build();
--- src/main/java/com/tweetarchive/main/repository/CollectionRepository.java
+++ src/main/java/com/tweetarchive/main/repository/CollectionRepository.java
@@ -4,7 +4,6 @@ import com.tweetarchive.main.model.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
