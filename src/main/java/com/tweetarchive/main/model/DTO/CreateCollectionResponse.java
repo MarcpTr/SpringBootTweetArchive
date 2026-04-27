3b73310cc3ed2b69028b08960c2d3959cc839ee3
marcphtr <marcphtr@gmail.com>
2026-04-27 14:39:52 +0200

    updated

--- src/main/java/com/tweetarchive/main/model/DTO/CreateCollectionResponse.java
+++
@@ -1,4 +0,0 @@
-package com.tweetarchive.main.model.DTO;
-
-public record CreateCollectionResponse(long id, String name, boolean isPublic) {
-}
--- src/main/java/com/tweetarchive/main/model/DTO/VisibilityRequest.java
+++
@@ -1,5 +0,0 @@
-package com.tweetarchive.main.model.DTO;
-
-public record VisibilityRequest(
-    boolean visible
-) {}
